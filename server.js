c55e72e3ab24ca08e81845acf4c197b2a90efb40
azer <azer@azers-MacBook-Pro.local>
2022-10-29 11:21:23 +0400

    login

--- server.js
+++ server.js
@@ -6,12 +6,28 @@
 // server.listen(3000,()=>console.log('Server listening...'))
 
 import express from "express";
-
+import cookieParser from "cookie-parser";
 const app = express();
 
 //request bodysini bu midleware ile jsona cevirmek ucundu
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use(cookieParser())
+
+app.use('/create',(req,res,next)=>{
+ const token=req.cookies.token
+
+ if(token){
+  next()
+
+ }else{
+  res.redirect('/login')
+ }
+})
+
+
+
 
 //view engin template ucun bucur set etmek lazimdi
 app.set("view engine", "hbs");
@@ -25,11 +41,20 @@ const posts = [
   },
 ];
 
+const users=[
+  {username:'admin',password:'admin'}
+]
+
+app.get('/login',(req,res)=>{
+  res.render('login')
+})
+
 app.get("/", (req, res) => {
   
 
   res.render("index", {
-    posts
+    posts,
+    isAuth:Boolean(req.cookies.token)?'log out':'log in'
   });
 });
 
@@ -58,5 +83,18 @@ app.post("/submit", (req, res) => {
  
   res.redirect('/')
 });
+
+app.post('/submit-login',(req,res)=>{
+  const {username,password}=req.body
+
+  if(users.find(u=>u.username===username && u.password===password)){
+    const token='token'
+    res.cookie('token',token,{maxAge:360000})
+    res.redirect('/')
+  }
+  else{
+    res.redirect('/login?error=true')
+  }
+})
 
 app.listen(3000, () => console.log("server listening.."));
